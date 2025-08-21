19247d0b95d63d8bd107bcb666fdec98eb70128b
Layiwola Tobi <33988842+Laytix@users.noreply.github.com>
2025-08-21 12:02:34 +0100

    4.13 & 4.14

--- part4/blogList/backend/controllers/blogs.js
+++ part4/blogList/backend/controllers/blogs.js
@@ -1,29 +1,37 @@
-const blogsRouter = require('express').Router()
-const { response } = require('express')
-const Blog = require('../models/blog')
+const blogsRouter = require("express").Router();
+const { response } = require("express");
+const Blog = require("../models/blog");
 
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
-blogsRouter.get('/', async(request, response) => {
+blogsRouter.post("/", async (request, response) => {
+  const blog = new Blog(request.body);
 
-  const blogs = await Blog.find({})
-  response.json(blogs)
-
-})
-
-blogsRouter.post('/', async(request, response) => {
-  const blog = new Blog(request.body)
-
-  if(!request.body.url || !request.body.title){
+  if (!request.body.url || !request.body.title) {
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const newBlog = await blog.save()
-  response.status(201).json(newBlog)
-})
+  const newBlog = await blog.save();
+  response.status(201).json(newBlog);
+});
 
-blogsRouter.delete('/:id', async(request, response) => {
-  
-})
-module.exports = blogsRouter
+blogsRouter.delete('/:id', async (request, response, next) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    request.body,
+    { new: true }
+  );
+  response.status(200).json(updatedBlog);
+});
+
+module.exports = blogsRouter;
--- part4/blogList/backend/tests/blog_api.test.js
+++ part4/blogList/backend/tests/blog_api.test.js
@@ -40,35 +40,56 @@ test("post is saved to database", async () => {
     author: "Brandon Sanderson",
     title: "TRESS OF THE EMERALD SEA",
     likes: 30,
-    url: "https://www.brandonsanderson.com/pages/standalones-cosmere"
-
-  }
+    url: "https://www.brandonsanderson.com/pages/standalones-cosmere",
+  };
 
   await api
-    .post('/api/blogs')
+    .post("/api/blogs")
     .send(newBlog)
-    .expect('Content-Type', /application\/json/)
-  
-  const response = await blogsInDB()
-  console.log(response)
-  assert.strictEqual(response.length, blogDummyData.length + 1)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await blogsInDB();
+  console.log(response);
+  assert.strictEqual(response.length, blogDummyData.length + 1);
 });
 
-test("blog with missing title or url isnt saved to database", async() =>{
-    const newBlog = {
+test("blog with missing title or url isnt saved to database", async () => {
+  const newBlog = {
     author: "Brandon Sanderson",
     likes: 30,
-  }
+  };
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+  await api.post("/api/blogs").send(newBlog).expect(400);
+
+  const blogsatEnd = await blogsInDB();
+  assert.strictEqual(blogsatEnd.length, blogDummyData.length);
+});
+
+test("individual blog is deleted from database", async () => {
+  const blogs = await blogsInDB();
+
+  const blogtodelete = blogs[0];
+  await api.delete(`/api/blogs/${blogtodelete.id}`).expect(204);
 
   const blogsatEnd = await blogsInDB()
-  assert.strictEqual(blogsatEnd.length, blogDummyData.length)
+  assert.strictEqual(blogsatEnd.length, blogDummyData.length - 1)
+});
 
-})
+test("a blog can be updated", async () => {
+  const blogsAtStart = await blogsInDB();
+  const blogToUpdate = blogsAtStart[0];
+  const updatedBlogData = { likes: blogToUpdate.likes + 1 };
+
+  await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(updatedBlogData)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await blogsInDB();
+  const updatedBlog = blogsAtEnd.find(blog => blog.id === blogToUpdate.id);
+  assert.strictEqual(updatedBlog.likes, blogToUpdate.likes + 1);
+});
 
 after(async () => {
   mongoose.connection.close();
