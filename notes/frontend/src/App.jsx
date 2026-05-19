f445554ac73cf488461380cd03bfe1cec1715997
Layiwola Tobi <33988842+Laytix@users.noreply.github.com>
2026-05-19 21:22:51 +0100

    Added react router to Notes
    Added react router to notes and created new pages, Home, Note and Note List.

--- notes/frontend/src/App.jsx
+++ notes/frontend/src/App.jsx
@@ -1,150 +1,55 @@
 import { useState, useEffect, useRef } from "react";
-import Note from "./components/Note";
 import noteService from "./services/notes";
-import Notification from "./components/Notification";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import NoteList from "./components/NoteList";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
-import loginService from "./services/login";
-import LoginForm from "./components/LoginForm";
-import Togglable from "./components/Togglable";
 import NoteForm from "./components/NoteForm";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("");
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null);
-
-  const [loginVisible, setLoginVisible] = useState(false);
-
-  useEffect(
-    () =>
-      noteService.getAll().then((initialNotes) => {
-        setNotes(initialNotes);
-      }),
-    []
-  );
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem("loggedNoteAppUser");
-
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON);
-      setUser(user);
-      noteService.setToken(user.token);
-    }
+    noteService
+      .getAll()
+      .then((initialNotes) => setNotes(initialNotes))
+      .catch((err) => console.error("Failed to fetch notes:", err));
   }, []);
 
-  const noteFormRef = useRef();
 
   const addNote = (noteObject) => {
-    noteFormRef.current.toggleVisibility()
+    noteFormRef.current.toggleVisibility();
     noteService.create(noteObject).then((returnedNote) => {
       setNotes(notes.concat(returnedNote));
       setNewNote("");
     });
   };
 
-  const toggleImportanceOf = (id) => {
-    const note = notes.find((n) => n.id === id);
-    const changedNote = { ...note, important: !note.important };
-
-    noteService
-      .update(id, changedNote)
-      .then((returnedNote) => {
-        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
-      })
-      .catch((error) => {
-        setErrorMessage(
-          `Note '${note.content}' was already removed from server due to ${error}`
-        );
-        setTimeout(() => {
-          setErrorMessage(null);
-        }, 5000);
-        alert(`the note "${note.content}" was already deleted from the server`);
-        setNotes(notes.filter((n) => n.id !== id));
-      });
+  const padding = {
+    padding: 5,
   };
-
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value);
-  };
-
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    console.log(`logging in with ${username} ${password}`);
-
-    try {
-      const user = await loginService.login({ username, password });
-      window.localStorage.setItem("loggedNoteAppUser", JSON.stringify(user));
-      noteService.setToken(user.token);
-      setUser(user);
-      setUsername("");
-      setPassword("");
-    } catch (error) {
-      setErrorMessage("wrong credentials");
-      setTimeout(() => {
-        setErrorMessage(null);
-      }, 5000);
-    }
-  };
-
-  const handleLogout = async() => {
-    event.preventDefault()
-    try{
-      window.localStorage.removeItem('loggedNoteAppUser')
-      setUser(null)
-    }catch{
-      console.log("error logging out")
-    }
-  }
-
-  const loginForm = () => {
-    return (
-      <Togglable buttonLabel="login">
-        <LoginForm
-          username={username}
-          password={password}
-          handleUsernameChange={({ target }) => setUsername(target.value)}
-          handlePasswordChange={({ target }) => setPassword(target.value)}
-          handleSubmit={handleLogin}
-        />
-      </Togglable>
-    );
-  };
-
-  const noteForm = () => (
-    <Togglable buttonLabel="New Note" ref={noteFormRef}>
-      <NoteForm createNote={addNote}/>
-    </Togglable>
-  );
 
   return (
-    <div>
-      <h1>Notes</h1>
-      <Notification message={errorMessage} />
+    <Router>
+      <div>
+        <Link style={padding} to="/">
+          Home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/create">
+          new note
+        </Link>
+      </div>
 
-      {!user && loginForm()}
-      {user && (
-        <div>
-          <p>{user.name} logged in</p>
-          <button onClick={handleLogout}>Logout</button>
-          {noteForm()}
-        </div>
-      )}
-
-      <ul>
-        {notes.map((note) => (
-          <Note
-            key={note.id}
-            note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)}
-          />
-        ))}
-      </ul>
+      <Routes>
+        <Route path="/notes" element={<NoteList notes={notes} />} />
+        <Route path="/create" element={<NoteForm createNote={addNote} />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 };
 export default App;
